ee5aa9da28d1ed1bb25fb1c241a0bddf33a86536
이현정 <1212guswjd@gmail.com>
2022-06-22 16:35:06 +0900

    apply refactory

--- member_and_order/core/src/main/java/hello/core/order/AppConfig.java
+++ member_and_order/core/src/main/java/hello/core/order/AppConfig.java
@@ -1,16 +1,27 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
-public class AppConfig {
-    public MemberService memberService() { // 구현 객체가 생성
-        return new MemberServiceImpl(new MemoryMemberRepository());
+public class AppConfig { // 리펙토리 : 메소드 이름에서 역할이 모두 보임
+    // 중복된 객체 생성 모음
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public MemberService memberService() { // 구현 객체 선택
+        return new MemberServiceImpl(memberRepository());
         // MemoryMemberRepository() : 역할에 해당하는 배우 선택
     }
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+
+    public OrderService orderService() { // 구현 객체 선택
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
