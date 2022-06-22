139a1873993e4ee99690738f980ad3beac72d12c
이현정 <1212guswjd@gmail.com>
2022-06-22 17:19:59 +0900

    modify java to spring!

--- member_and_order/core/src/main/java/hello/core/MemberApp.java
+++ member_and_order/core/src/main/java/hello/core/MemberApp.java
@@ -5,12 +5,21 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService =  applicationContext.getBean("memberService", MemberService.class);
+        // 인자 : "클래스 이름", 반환 타입
+
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+
         //MemberService memberService = new MemberServiceImpl();
+
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- member_and_order/core/src/main/java/hello/core/order/AppConfig.java
+++ member_and_order/core/src/main/java/hello/core/order/AppConfig.java
@@ -7,20 +7,27 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { // 리펙토리 : 메소드 이름에서 역할이 모두 보임
     // 중복된 객체 생성 모음
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public MemberService memberService() { // 구현 객체 선택
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() { // 구현 객체 선택
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
         // fix -> rate 배우 교체시 변경하는 부분
